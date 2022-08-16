77865826fafcdfb638dd3d4f421ef96f03c75a7c
David Marino <98052934+dmarino8@users.noreply.github.com>
2022-08-16 15:54:11 -0500

    Delete database-search-api directory

--- database-search-api/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-
-insert into keywords(id, keyword) values (1, 'loop');
-
-insert into results(res_id, result, key_id) values(1, 'do while', 1);
-insert into results(res_id, result, key_id) values(2, 'while', 1);
-insert into results(res_id, result, key_id) values(3, 'for', 1);
