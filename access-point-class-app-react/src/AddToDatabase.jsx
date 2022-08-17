a1fc3a525c6fec4a9452033e5907a43496379eae
Spencer McNeely <spencerm5190@gmail.com>
2022-08-17 15:50:28 -0500

    post endpoint working on the UI

--- access-point-class-app-react/src/AddToDatabase.jsx
+++ access-point-class-app-react/src/AddToDatabase.jsx
@@ -3,26 +3,27 @@ import React, {useState, useEffect} from "react";
 
 const AddToDatabase = () => {
 
-    const [input, setInput] = useState ("");
+    const [input, setInput] = useState (null);
 
-    const data = {
-        keyword : input,
+    const keyword = {
+        keyword : input
         };
 
-     const sendRequest = (data) => {
+     const sendRequest = (keyword) => {
             fetch("http://localhost:8090/api/keywords", {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(data)
+                body: JSON.stringify(keyword)
             }).then(res => {
                 console.log("Request complete! response:", res);
+                console.log(keyword);
             });
         }
     return (
         <div>
             <label for="keyword">Add To Topics</label><br></br>
-            <input type="text" id="keyword" name="keyword"/><br></br>
-            <button onClick={() => setInput("keyword")}>Add To Topics</button>
+            <input type="text" id="keyword" name="keyword" onInput={e => setInput(e.target.value)}/><br></br>
+            <button onClick={() => sendRequest(keyword)}>Add To Topics</button>
             <button>Clear</button>
             <br></br>
             
