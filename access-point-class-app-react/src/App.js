b2ab9c5b6ef352b7fb0438fa45588b398e35f623
Tori Willis <twillis75@stlcc.edu>
2022-08-17 13:58:53 -0500

    Update

--- access-point-class-app-react/src/App.js
+++ access-point-class-app-react/src/App.js
@@ -31,7 +31,7 @@ function App() {
 
     return (
         <div>
-            {loginState === true ?
+            {loginState === false ?
                 <Login setLoginState={setLoginState}/>
                 : <Tab/>
             }
