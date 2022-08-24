89f9ef511465646e37c41015a70ba65983ab7341
Abby Schlote <84693724+aschlote3@users.noreply.github.com>
2022-08-24 09:30:39 -0500

    Update Login.jsx

--- access-point-class-app-react/src/Login.jsx
+++ access-point-class-app-react/src/Login.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import "./Loginstyle.css";
- 
+
 const Login = ({ setLoginState }) => {
- 
+
     const user = "username";
     const pass = "password";
     const [userState, setUserState] = useState(null);
     const [passState, setPassState] = useState(null);
- 
- 
+
     const checkCredentials = () => {
         if (userState !== user && passState !== pass) {
             alert("login fail : invalid username and password")
@@ -24,20 +23,20 @@ const Login = ({ setLoginState }) => {
             return true;
         }
     }
- 
+
     const doLogin = () => {
         setLoginState(checkCredentials());
     }
- 
     const clear = () => {
         setUserState('');
         setPassState('');
     }
- 
+
     return (
         <div className="loginContainer">
+            <img src="https://www.pngitem.com/pimgs/m/256-2564932_bayer-logo-png-transparent-png.png" width="200" height="200"></img>
             <h1>Access Point Class of 2022</h1>
-            <h3>Let's Go Get It</h3><br></br>
+            <h3>Let's Go Get It</h3>
             <label for="username">Username</label><br></br>
             <input autocomplete="on" type="text" id="username" name="username" className="username" onInput={e => setUserState(e.target.value)} value={userState} /><br></br>
             <label for="password">Password</label><br></br>
@@ -47,5 +46,5 @@ const Login = ({ setLoginState }) => {
         </div>
     );
 }
- 
+
 export default Login;
